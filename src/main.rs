0fdee7c2db94edac821793313f840b5443c12729
GothardTA <166161353+GothardTA@users.noreply.github.com>
2024-11-09 16:34:19 -0700

    it works (without a camera)
    my camera broke so this code plays a random move sadly. You should be able to un-comment parts of the code to re-enable the camera functionality

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,156 @@
 use rand::Rng;
-use image::{io::Reader as ImageReader, Pixel};
+use image::{ImageReader, Pixel};
 use std::{process::Command, thread, time::Duration};
-use gpio::{GpioOut, GpioIn};
+use std::error::Error;
 
-fn main() {
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
+
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Connect Four Robot v0.1.0 - MIT license - see https://github.com/GothardTA/connectfourrobot for more details");
-
-    let mut gpio23 = gpio::sysfs::SysFsGpioOutput::open(23).unwrap();
-    let mut gpio24 = gpio::sysfs::SysFsGpioOutput::open(24).unwrap();
-    let mut gpio25 = gpio::sysfs::SysFsGpioOutput::open(25).unwrap();
-    let mut gpio12 = gpio::sysfs::SysFsGpioInput::open(12).unwrap();
-
-    // calls the pi's libcamera-still command to take a picture and save it to a file
-    let output = Command::new("bash").arg("takepic.sh").output().expect("Failed to take picture");
+    println!("Currently running on a {}", DeviceInfo::new()?.model());
     
-    let msg = match String::from_utf8(output.stdout) {
-        Ok(v) => v,
-        Err(e) => String::from("Error".to_owned() + &e.to_string()),
-    };
-    println!("{}", msg);
-    println!("Picture Captured and saved as image.jpg");
+    let mut gpio23 = Gpio::new()?.get(23)?.into_output();
+    let mut gpio24 = Gpio::new()?.get(24)?.into_output();
+    let mut gpio25 = Gpio::new()?.get(25)?.into_output();
+    let mut gpio12 = Gpio::new()?.get(12)?.into_input_pullup();
+    println!("Initialized gpio pins");
+    println!("Waiting for button input...");
 
-    let img = ImageReader::open("image.jpg").expect("Failed to open file").decode().expect("Failed to decode image").into_rgba8();
-    
-    let positions = [
-        [[24, 714], [141, 725], [237, 730], [320, 718], [380, 721], [434, 717], [482, 716]],
-        [[90, 532], [195, 571], [284, 585], [353, 601], [411, 612], [459, 616], [502, 626]],
-        [[138, 390], [241, 430], [320, 463], [376, 499], [435, 513], [476, 535], [519, 547]],
-        [[181, 253], [273, 307], [350, 354], [408, 391], [455, 428], [498, 445], [537, 469]],
-        [[222, 105], [307, 190], [378, 249], [434, 283], [475, 342], [515, 368], [553, 385]],
-        [[257, 13], [340, 84], [402, 160], [453, 212], [503, 258], [535, 284], [575, 320]]
-    ];
-
-    let mut board: [[u8; 7]; 6] = [[0; 7]; 6];
-
-    for row in 0..positions.len() {
-        for col in 0..positions[0].len() {
-            let rg_difference = 
-                (img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[0] as i32 -
-                img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[1] as i32).abs();
+    loop {
+        if gpio12.is_low() {
+            // calls the pi's libcamera-still command to take a picture and save it to a file
+            // let output = Command::new("bash").arg("takepic.sh").output().expect("Failed to take picture");
             
-            let rb_difference = 
-                (img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[0] as i32 -
-                img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[2] as i32).abs();
+            // let msg = match String::from_utf8(output.stdout) {
+            //     Ok(v) => v,
+            //     Err(e) => String::from("Error".to_owned() + &e.to_string()),
+            // };
+            // println!("{}", msg);
+            println!("Picture Captured and saved as image.jpg");
 
-            let gb_difference = 
-                (img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[1] as i32 -
-                img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[2] as i32).abs();
-            
-            if rg_difference > 40 && rb_difference > 40 && gb_difference <= 80 {
-                board[row][col] = b'R';
-            } else if rg_difference <= 40 && rb_difference > 40 && gb_difference > 40 {
-                board[row][col] = b'Y';
-            } else {
-                board[row][col] = b' ';
-            }
-        }
-    }
+            // let img = ImageReader::open("/home/pi/image.jpg").expect("Failed to open file").decode().expect("Failed to decode image").into_rgba8();
+            println!("SUccesfully opened image!");
 
-    display_board(&board);
+            let positions = [
+                [[24, 714], [141, 725], [237, 730], [320, 718], [380, 721], [434, 717], [482, 716]],
+                [[90, 532], [195, 571], [284, 585], [353, 601], [411, 612], [459, 616], [502, 626]],
+                [[138, 390], [241, 430], [320, 463], [376, 499], [435, 513], [476, 535], [519, 547]],
+                [[181, 253], [273, 307], [350, 354], [408, 391], [455, 428], [498, 445], [537, 469]],
+                [[222, 105], [307, 190], [378, 249], [434, 283], [475, 342], [515, 368], [553, 385]],
+                [[257, 13], [340, 84], [402, 160], [453, 212], [503, 258], [535, 284], [575, 320]]
+            ];
 
-    for _iter in 0..6 {
-        for col in 0..7 {
-            for row in 0..5 {
-                if board[row][col] != b' ' && board[row + 1][col] == b' ' {
-                    board[row][col] = b' ';
+            let mut board: [[u8; 7]; 6] = [[0; 7]; 6];
+
+            // for row in 0..positions.len() {
+            //     for col in 0..positions[0].len() {
+            //         let rg_difference = 
+            //             (img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[0] as i32 -
+            //             img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[1] as i32).abs();
+                    
+            //         let rb_difference = 
+            //             (img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[0] as i32 -
+            //             img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[2] as i32).abs();
+
+            //         let gb_difference = 
+            //             (img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[1] as i32 -
+            //             img.get_pixel(positions[row][col][0], positions[row][col][1]).to_rgb()[2] as i32).abs();
+                    
+            //         if rg_difference > 40 && rb_difference > 40 && gb_difference <= 80 {
+            //             board[row][col] = b'R';
+            //         } else if rg_difference <= 40 && rb_difference > 40 && gb_difference > 40 {
+            //             board[row][col] = b'Y';
+            //         } else {
+            //             board[row][col] = b' ';
+            //         }
+            //     }
+            // }
+            for row in 3..positions.len() {
+                for col in 0..positions[0].len() {
+                    let mut rng = rand::thread_rng();
+                    let num = rng.gen_range(0..2);
+
+                    if num == 0 {
+                        board[row][col] = b'R';
+                    } else if num == 1 {
+                        board[row][col] = b'Y';
+                    } else {
+                        board[row][col] = b' ';
+                    }
                 }
             }
+            for col in 0..positions[0].len() {
+                let mut rng = rand::thread_rng();
+                let num = rng.gen_range(0..4);
+
+                if num == 0 {
+                    board[2][col] = b'R';
+                } else if num == 1 {
+                    board[2][col] = b'Y';
+                } else {
+                    board[2][col] = b' ';
+                }
+            }
+            
+
+            display_board(&board);
+
+            for _iter in 0..6 {
+                for col in 0..7 {
+                    for row in 0..5 {
+                        if board[row][col] != b' ' && board[row + 1][col] == b' ' {
+                            board[row][col] = b' ';
+                        }
+                    }
+                }
+            }
+
+            display_board(&board);
+            let col = ai_move(&mut board, 'Y');
+            println!("{}", col + 1);
+
+            if col == 0 {
+                gpio23.set_low();
+                gpio25.set_low();
+                gpio24.set_high();
+            } else if col == 1 {
+                gpio23.set_low();
+                gpio25.set_high();
+                gpio24.set_low();
+            } else if col == 2 {
+                gpio23.set_low();
+                gpio25.set_high();
+                gpio24.set_high();
+            } else if col == 3 {
+                gpio23.set_high();
+                gpio25.set_low();
+                gpio24.set_low();
+            } else if col == 4 {
+                gpio23.set_high();
+                gpio25.set_low();
+                gpio24.set_high();
+            } else if col == 5 {
+                gpio23.set_high();
+                gpio25.set_high();
+                gpio24.set_low();
+            } else if col == 6 {
+                gpio23.set_high();
+                gpio25.set_high();
+                gpio24.set_high();
+            }
+
+            thread::sleep(Duration::from_millis(1000));
+            gpio23.set_low();
+            gpio25.set_low();
+            gpio24.set_low();
+            
+            println!("Sent value over cable");
         }
     }
 
-    display_board(&board);
-    let col = ai_move(&mut board, 'Y');
-    println!("{}", col + 1);
-
-    if col == 0 {
-        gpio23.set_value(false).expect("could not set gpio23");
-        gpio25.set_value(false).expect("could not set gpio25");
-        gpio24.set_value(true).expect("could not set gpio24");
-    } else if col == 1 {
-        gpio23.set_value(false).expect("could not set gpio23");
-        gpio25.set_value(true).expect("could not set gpio25");
-        gpio24.set_value(false).expect("could not set gpio24");
-    } else if col == 2 {
-        gpio23.set_value(false).expect("could not set gpio23");
-        gpio25.set_value(true).expect("could not set gpio25");
-        gpio24.set_value(true).expect("could not set gpio24");
-    } else if col == 3 {
-        gpio23.set_value(true).expect("could not set gpio23");
-        gpio25.set_value(false).expect("could not set gpio25");
-        gpio24.set_value(false).expect("could not set gpio24");
-    } else if col == 4 {
-        gpio23.set_value(true).expect("could not set gpio23");
-        gpio25.set_value(false).expect("could not set gpio25");
-        gpio24.set_value(true).expect("could not set gpio24");
-    } else if col == 5 {
-        gpio23.set_value(true).expect("could not set gpio23");
-        gpio25.set_value(true).expect("could not set gpio25");
-        gpio24.set_value(false).expect("could not set gpio24");
-    } else if col == 6 {
-        gpio23.set_value(true).expect("could not set gpio23");
-        gpio25.set_value(true).expect("could not set gpio25");
-        gpio24.set_value(true).expect("could not set gpio24");
-    }
-
-    thread::sleep(Duration::from_millis(1000));
-    gpio23.set_value(false).expect("could not set gpio23");
-    gpio25.set_value(false).expect("could not set gpio25");
-    gpio24.set_value(false).expect("could not set gpio24");
     
-    println!("Sent value over cable");
+    Ok(())
 }
 
 // outputs the board to the screen
@@ -337,6 +378,10 @@ fn check_two_in_a_row(board: &[[u8; 7]; 6], player: char) -> bool {
 }
 
 fn ai_move(board: &mut [[u8; 7]; 6], player: char) -> u8 {
+    let mut rng = rand::thread_rng();
+    let num = rng.gen_range(0..7);
+    return num;
+    
     let mut tmp_board: [[u8; 7]; 6];
     let other_player: char;
 
